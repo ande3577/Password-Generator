5a4b546322ec9092e56ba8b20ece8f6bda7ad3a5
David S Anderson <ande3577@gmail.com>
2012-01-18 22:13:59 -0600

    keywords will randomly select numbers that resemble letters.

--- org.dsanderson.password_generator/src/org/dsanderson/password_generator/core/CompoundCharacterScrambler.java
+++ org.dsanderson.password_generator/src/org/dsanderson/password_generator/core/CompoundCharacterScrambler.java
@@ -21,6 +21,11 @@ public class CompoundCharacterScrambler implements IRandomCharacterGenerator {
 				characterScramblers.add(new RandomCharacterGenerator(Character
 						.toString(Character.toLowerCase(ch)),
 						settings.lowerCaseWeight, true));
+			if (settings.numericEnabled)
+				characterScramblers.add(new RandomCharacterGenerator(
+						getNumericString(ch), settings.numericWeight,
+						!settings.upperCaseEnabled
+								&& !settings.lowerCaseEnabled));
 		} else {
 			characterScramblers.add(new RandomCharacterGenerator(Character
 					.toString(ch), 1, true));
@@ -52,6 +57,31 @@ public class CompoundCharacterScrambler implements IRandomCharacterGenerator {
 	@Override
 	public int RequiredLength() {
 		return 1;
+	}
+
+	private String getNumericString(char ch) {
+		switch ((int) Character.toLowerCase(ch)) {
+		case 'i':
+		case 'l':
+			return "1";
+		case 'o':
+			return "0";
+		case 'e':
+			return "3";
+		case 'y':
+			return "4";
+		case 's':
+			return "5";
+		case 'b':
+			return "6B";
+		case 'v':
+			return "7";
+		case 'g':
+			return "9";
+
+		default:
+			return "";
+		}
 	}
 
 }
