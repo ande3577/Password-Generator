e136ff2ee0cbf7fe277d5d0550a0f80eaffa6265
David S Anderson <ande3577@gmail.com>
2012-01-26 22:34:40 -0600

    added swing and console

---
+++ org.dsanderson.password_generator.console/src/org/dsanderson/password_generator/ConsoleApp.java
@@ -0,0 +1,57 @@
+/**
+ * @author David S Anderson
+ * 
+ * Copyright (C) 2012
+ */
+package org.dsanderson.password_generator;
+
+import org.dsanderson.password_generator.core.*;
+
+public class ConsoleApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		UserSettings settings = UserSettings.getInstance();
+
+		for (int i = 0; i < args.length; i++) {
+			String parameterName[] = args[i].split("=");
+
+			if (parameterName[0].matches("--version")) {
+				System.out.println(ProgramInfo.programName);
+				System.out.println(ProgramInfo.programVersion);
+				System.out.println(ProgramInfo.author);
+				System.out.println(ProgramInfo.copyright);
+				System.exit(0);
+			}
+			if (parameterName[0].matches("--length")) {
+				settings.passwordLength = Integer.parseInt(parameterName[1]);
+			} else if (parameterName[0].matches("--uppercase")) {
+				settings.upperCaseEnabled = !parameterName[1].matches("false");
+			} else if (parameterName[0].matches("--uppercaseweight")) {
+				settings.upperCaseWeight = Integer.parseInt(parameterName[1]);
+			} else if (parameterName[0].matches("--lowercase")) {
+				settings.lowerCaseEnabled = !parameterName[1].matches("false");
+			} else if (parameterName[0].matches("--lowercaseweight")) {
+				settings.lowerCaseWeight = Integer.parseInt(parameterName[1]);
+			} else if (parameterName[0].matches("--number")) {
+				settings.numericEnabled = !parameterName[1].matches("false");
+			} else if (parameterName[0].matches("--numberweight")) {
+				settings.numericWeight = Integer.parseInt(parameterName[1]);
+			} else if (parameterName[0].matches("--special")) {
+				settings.specialEnabled = !parameterName[1].matches("false");
+			} else if (parameterName[0].matches("--specialweight")) {
+				settings.specialWeight = Integer.parseInt(parameterName[1]);
+			} else if (parameterName[0].matches("--keyword")) {
+				settings.keyword = parameterName[1];
+			}
+		}
+
+		PasswordGenerator generator = new PasswordGenerator();
+
+		System.out.println(generator.GeneratePassword());
+		System.exit(0);
+	}
+}
